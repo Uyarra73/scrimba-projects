c409d61ddbd25499adc355006adcf2778d6e8747
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2023-11-27 17:58:11 +0900

    First scrimba project

---
+++ index.js
@@ -0,0 +1,22 @@
+ let countEl = document.getElementById("count-el");
+ let count = 0;
+ let saveEl = document.getElementById("save-el");
+
+ function increment(){
+    count += 1;
+    countEl.textContent = count;
+ }
+
+ function save(){
+   let num = count + " - ";
+    saveEl.textContent += num;
+    countEl.textContent = 0;
+    count = 0;
+ }
+
+save();
+
+
+
+
+
